085fdebec15ee46270504cdeea560204cdf96f83
Joshua Barbosa <joshuaslbarbosa@gmail.com>
2022-08-03 18:57:06 -0300

    ajustes

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -10,28 +10,28 @@
         <link rel="apple-touch-icon" href="{{ asset('storage/images/icon.png') }}" />
         <meta name="msapplication-TileImage" content="{{ asset('storage/images/icon.png') }}" />
        
-       <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6R8JZ4DRYT"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6R8JZ4DRYT"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-  gtag('config', 'G-6R8JZ4DRYT');
-</script>
+        gtag('config', 'G-6R8JZ4DRYT');
+        </script>
 
         <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WZM7MRW');</script>
-<!-- End Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WZM7MRW');</script>
+        <!-- End Google Tag Manager -->
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZM7MRW"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZM7MRW"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
 
         <meta name="title" content="{{ Config::get('app.name') }}">
         <meta name="description" content="{{ Config::get('app.name') }}">
@@ -40,7 +40,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         @stack('header')
         <link rel="stylesheet" href="{{ asset('mdb/css/mdb.min.css') }}">
         <link rel="stylesheet" href="{{ asset('fontawesome/css/all.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css?v=0.0.3.0') }}">
         @stack('styles')
     </head>
     <body>
@@ -50,7 +50,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <x-footer />
         @yield('modals')
         <script src="{{ asset('mdb/js/mdb-pt_BR.js') }}"></script>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/app.js?v=0.0.3.0') }}"></script>
         @stack('scripts')
     </body>
 </html>
